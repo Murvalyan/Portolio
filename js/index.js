6a3a2a194360b832f57c52e3cd5b9642f21473c4
Murvalyan <Murvalyans@gmail.com>
2022-03-04 11:14:19 +0700

    header

---
+++ js/index.js
@@ -0,0 +1,12 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'vertical',
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  noSwipingClass: 'swiper-no-swiping'
+
+});
